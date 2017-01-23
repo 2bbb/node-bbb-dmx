2ef284aa52eca6aee905900d1db5efd22303d0d3
ISHII 2bit <ishii@buffer-renaiss.com>
2017-01-24 01:41:31 +0900

    add chase and color

--- bbb-dmx.js
+++ bbb-dmx.js
@@ -3,7 +3,9 @@ const bbb = require('bit-by-bit');
 
 const bbb_dmx = lazier({
 	artnet: './lib/artnet.js',
-	dmx: './lib/dmx.js'
+	dmx: './lib/dmx.js',
+	color: './lib/color.js',
+	chase: './lib/chase.js'
 }, __dirname);
 bbb_dmx.functional = bbb.functional;
 bbb_dmx.osc = bbb.osc;
